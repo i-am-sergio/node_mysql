08716ae19bc9e69ec0c89fa77690c31f691d2287
sergio15dev <sergio15dev@gmail.com>
2022-12-19 22:04:17 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { pool } from './db.js'
+import { PORT } from './config.js'
+
+const app = express()
+
+app.get('/', async (req,res) => {
+  const [rows] = await pool.query("SELECT * FROM users")
+  res.json(rows);
+  //res.send("Welcome to server")
+})
+
+app.get('/ping', async (req,res) => {
+  const [result] = await pool.query('SELECT "hello world" AS RESULT');
+  res.json(result[0]) 
+  //res.send("Welcome to server")
+})
+
+app.get('/create', async (req,res) => {
+  const result = await pool.query('INSERT INTO users(name) VALUES ("John")')
+  res.json(result)
+})
+
+app.listen(PORT)
+console.log("Server on port",PORT);
